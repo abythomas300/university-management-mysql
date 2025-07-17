2f33d92e7fd182ecc7e12ace96e5687ffa845c0b
abythomas300 <abythomas300@gmail.com>
2025-07-18 01:32:36 +0530

    schema design: departments table

--- changes.sql
+++ changes.sql
@@ -1,2 +1,11 @@
 /*Create Database*/
 CREATE DATABASE universityDB
+
+/*- - - SCHEMA DESIGN - - - */
+/*departments table*/
+CREATE TABLE departments(
+	department_id INT AUTO_INCREMENT,
+    department_name VARCHAR(255) NOT NULL UNIQUE,
+    department_head VARCHAR(255),
+    PRIMARY KEY(department_id)
+);
