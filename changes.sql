e5eb0d68ff9bf11a65206f59aa4d3c73826892c4
abythomas300 <abythomas300@gmail.com>
2025-07-18 01:36:40 +0530

    schema design: students table

--- changes.sql
+++ changes.sql
@@ -9,3 +9,14 @@ CREATE TABLE departments(
     department_head VARCHAR(255),
     PRIMARY KEY(department_id)
 );
+/*students table*/
+CREATE TABLE students(
+ student_id INT AUTO_INCREMENT,
+ student_name VARCHAR(255) NOT NULL,
+ department INT NOT NULL,
+ gender ENUM('Male', 'Female'),
+ email VARCHAR(255) UNIQUE,
+ dob DATE NOT NULL,
+ PRIMARY KEY(student_id),
+ FOREIGN KEY(department) REFERENCES departments(department_id)
+);
